83ad770fe076751f1d9904c5cfa5f9e90f62f5f2
Vyjith <38276429+vyjith@users.noreply.github.com>
2022-09-25 17:18:53 +0530

    Revert "added some lines to the jenkins.sh file"

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-sudo yum update -y
-sudo amazon-linux-extras install epel -y
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade
-sudo amazon-linux-extras install java-openjdk11 -y
-sudo yum install jenkins -y
-sudo systemctl enable jenkins.service
-sudo systemctl start jenkins.service
+amazon-linux-extras install epel -y
+yum install java-1.8.0-openjdk-devel -y
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+yum install jenkins -y
+systemctl start jenkins.service
+systemctl enable jenkins.service
