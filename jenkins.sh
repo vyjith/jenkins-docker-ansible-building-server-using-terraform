ec8b04c183130b861acfd7b9457cc7eba8c4d566
vyjith <vyjithks123@gmail.com>
2022-09-25 17:12:01 +0530

    added some lines to the jenkins.sh file

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-amazon-linux-extras install epel -y
-yum install java-1.8.0-openjdk-devel -y
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-yum install jenkins -y
-systemctl start jenkins.service
-systemctl enable jenkins.service
+sudo yum update -y
+sudo amazon-linux-extras install epel -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins.service
+sudo systemctl start jenkins.service
